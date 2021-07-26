baacb420fd443c530624339337885c0172346e71
MvpAlej <84643955+MvpAlej@users.noreply.github.com>
2021-07-26 16:05:20 -0600

    V1.2
    • Added Spam Reporter

---
+++ MultiTool/eagle.py
@@ -0,0 +1,133 @@
+import os
+from colorama import Fore
+from colorama.ansi import clear_screen
+import sys
+import time
+
+
+
+def Loading():
+    print("██ 39%")
+    print("███ 49%")
+    print("█████ 89%")
+    time.sleep(0.5)
+    print("██████████████]99%")
+    time.sleep(0.5)
+    print("Finished. By stoned.eagle#0001 & kojimu#1916")
+
+
+
+
+Loading()
+
+
+
+
+
+
+time.sleep(1)
+
+
+
+
+
+os.system("cls && title Eagle Multi Tool")
+
+
+
+
+def Design():
+    print(f''' 
+    
+    
+    
+ {Fore.RED}______            _       {Fore.BLUE} __  __       _ _   _  {Fore.RED} _______          _ 
+ {Fore.RED}| ____|          | |      {Fore.BLUE}|  \/  |     | | | (_) {Fore.RED}|__   __|        | |
+ {Fore.RED} | |__   __ _  __ _| | ___{Fore.BLUE}  | \  / |_   _| | |_ _{Fore.RED}     | | ___   ___ | |
+ {Fore.RED} |  __| / _` |/ _` | |/ _ \{Fore.BLUE} | |\/| | | | | | __| |{Fore.RED}    | |/ _ \ / _ \| |
+ {Fore.RED}| |___| (_| | (_| | |  __/ {Fore.BLUE}| |  | | |_| | | |_| | {Fore.RED}   | | (_) | (_) | |
+ {Fore.RED}|______\__,_|\__, |_|\___| {Fore.BLUE}|_|  |_|\__,_|_|\__|_| {Fore.RED}   |_|\___/ \___/|_|
+               __/ |                                                 
+               
+               {Fore.YELLOW} By Stoned.eagle#0001
+               {Fore.WHITE} discord.gg/beamz
+
+
+               ''')           
+
+
+
+
+
+
+Design()
+
+
+print("")
+print(f'{Fore.RED}[{Fore.LIGHTCYAN_EX}1{Fore.LIGHTWHITE_EX}] {Fore.RED} Hacking Tools ')
+print(f'{Fore.RED}[{Fore.LIGHTCYAN_EX}2{Fore.LIGHTWHITE_EX}] {Fore.RED} Roblox Tools ')
+print(f'{Fore.RED}[{Fore.LIGHTCYAN_EX}3{Fore.LIGHTWHITE_EX}] {Fore.RED} Discord Tools ')
+
+
+
+{Fore.WHITE}
+
+
+
+answer = input("> ")
+
+if answer == "1":
+    print("opening")
+    time.sleep(0.8)
+    exec(open('hack.py').read())
+else:
+    print("Not an answer lol?")
+
+
+if answer == "2":
+    exec(open('roblox.py').read())
+else:
+    print("Not an answer lol?")
+
+
+if answer == "3":
+    exec(open('discord.py').read())
+else:
+    print("Not an answer lol?")
+
+
+
+
+
+
+
+
+def Design():
+    print(f''' 
+    
+    
+
+
+    
+ {Fore.RED}______            _       {Fore.BLUE} __  __       _ _   _  {Fore.RED} _______          _ 
+ {Fore.RED}| ____|          | |      {Fore.BLUE}|  \/  |     | | | (_) {Fore.RED}|__   __|        | |
+ {Fore.RED} | |__   __ _  __ _| | ___{Fore.BLUE}  | \  / |_   _| | |_ _{Fore.RED}     | | ___   ___ | |
+ {Fore.RED} |  __| / _` |/ _` | |/ _ \{Fore.BLUE} | |\/| | | | | | __| |{Fore.RED}    | |/ _ \ / _ \| |
+ {Fore.RED}| |___| (_| | (_| | |  __/ {Fore.BLUE}| |  | | |_| | | |_| | {Fore.RED}   | | (_) | (_) | |
+ {Fore.RED}|______\__,_|\__, |_|\___| {Fore.BLUE}|_|  |_|\__,_|_|\__|_| {Fore.RED}   |_|\___/ \___/|_|
+               __/ |                                                 
+               
+               {Fore.YELLOW} By Stoned.eagle#0003 & kojimu#1916
+               {Fore.WHITE} discord.gg/beamz
+
+
+               ''')           
+
+Design()
+
+
+
+os.system
+
+
+
